3997f9d7d7ddace65a1a0a431d3856974689c016
sondes <sondesboussif1997@gmail.com>
2019-05-02 17:32:20 +0100

    tp3

--- content.php
+++ content.php
@@ -1,6 +1,8 @@
 <div class="blog-post">
- <h2 class="blog-post-itle"><?php the_title(); ?></h2>
+ <h2 class="blog-post-title"><a href="<?php the_permalink(); ?>"><?php
+the_title(); ?></a></h2>
  <p class="blog-post-meta"><?php the_date(); ?> par <a
 href="#"><?php the_author(); ?></a></p>
- <?php the_content(); ?>
+ <?php the_excerpt(); ?>
 </div><!-- /.blog-post -->
+<a href="#" class="instagram text-center" style="<?php bloginfo('template_directory'); ?>background-image: url(images/p.jpg);">
--- sidebar.php
+++ sidebar.php
@@ -97,32 +97,15 @@
 						</div>
 						<div class="side-wrap">
 							<h2 class="sidebar-heading">Gallery</h2>
-							
 
-							<a href="images/gallery-1.jpg" class="gallery image-popup-link text-center" style="<?php bloginfo('template_directory'); ?>background-image: url(images/gallery-1.jpg);">
-								<span><i class="icon-search3"></i></span>
-							</a>
-							<a href="images/gallery-2.jpg" class="gallery image-popup-link text-center" style="<?php bloginfo('template_directory'); ?>background-image: url(images/gallery-2.jpg);">
-								<span><i class="icon-search3"></i></span>
-							</a>
-							<a href="images/gallery-3.jpg" class="gallery image-popup-link text-center" style="<?php bloginfo('template_directory'); ?>background-image: url(images/gallery-3.jpg);">
-								<span><i class="icon-search3"></i></span>
-							</a>
-							<a href="images/gallery-4.jpg" class="gallery image-popup-link text-center" style="<?php bloginfo('template_directory'); ?>background-image: url(images/gallery-4.jpg);">
-								<span><i class="icon-search3"></i></span>
-							</a>
-							<a href="images/gallery-5.jpg" class="gallery image-popup-link text-center" style="<?php bloginfo('template_directory'); ?>background-image: url(images/gallery-5.jpg);">
-								<span><i class="icon-search3"></i></span>
-							</a>
-							<a href="images/gallery-6.jpg" class="gallery image-popup-link text-center" style="<?php bloginfo('template_directory'); ?>background-image: url(images/gallery-6.jpg);">
-								<span><i class="icon-search3"></i></span>
-							</a>
-							<a href="images/gallery-7.jpg" class="gallery image-popup-link text-center" style="<?php bloginfo('template_directory'); ?>background-image: url(images/gallery-7.jpg);">
-								<span><i class="icon-search3"></i></span>
-							</a>
-							<a href="images/gallery-8.jpg" class="gallery image-popup-link text-center" style="<?php bloginfo('template_directory'); ?>background-image: url(images/gallery-8.jpg);">
-								<span><i class="icon-search3"></i></span>
-							</a>
+							<a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/p.jpg" alt=""></a>
+							<a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/gallery-2.jpg" alt=""></a>
+							<a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/gallery-3.jpg" alt=""></a>
+							<a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/gallery-4.jpg" alt=""></a>
+							<a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/gallery-5.jpg" alt=""></a>
+							<a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/gallery-6.jpg" alt=""></a>
+							<a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/gallery-7.jpg" alt=""></a>
+							<a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/gallery-8.jpg" alt=""></a>
 						</div>
 						<div class="side-wrap">
 							<h2 class="sidebar-heading">Blockquote</h2>
