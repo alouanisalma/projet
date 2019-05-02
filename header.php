deb30c6c6b2f1eff578a0a592ceb3d606a179817
Unknown <unknown@example.com>
2019-05-02 14:36:22 +0200

    tp2

---
+++ header.php
@@ -0,0 +1,177 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<div class="blog-header">
+ <h1 class="blog-title">
+ <a href="<?php bloginfo( 'wpurl' );?>"><?php echo
+get_bloginfo( 'name' ); ?></a></h1>
+ <p class="lead blog-description"><?php echo get_bloginfo(
+'description' ); ?></p>
+</div> 
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+
+  <!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	
+	<link href="https://fonts.googleapis.com/css?family=Grand+Hotel" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Fira+Sans:100,200,300,400,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
+	
+	<!-- Animate.css -->
+	<link href="<?php bloginfo('template_directory'); ?>/css/animate.css"
+rel="stylesheet">
+	<link href="<?php bloginfo('template_directory'); ?>/css/icomoon.css"
+rel="stylesheet">	
+	<!-- Icomoon Icon Fonts-->
+	<link href="<?php bloginfo('template_directory'); ?>/css/bootstrap.css"
+rel="stylesheet">
+	<!-- Bootstrap  -->
+	
+	<link href="<?php bloginfo('template_directory'); ?>/css/magnific-popup.css"
+rel="stylesheet">
+	<!-- Magnific Popup -->
+
+	<link href="<?php bloginfo('template_directory'); ?>/css/flexslider.css"
+rel="stylesheet">
+	<!-- Flexslider  -->
+	<link href="<?php bloginfo('template_directory'); ?>/css/owl.carousel.min.css"
+rel="stylesheet">	
+
+	<!-- Owl Carousel -->
+	<link href="<?php bloginfo('template_directory'); ?>/css/owl.theme.default.min.css"
+rel="stylesheet">
+
+	<link href="<?php bloginfo('template_directory'); ?>/fonts/flaticon/font/flaticon.css"
+rel="stylesheet">	
+	<!-- Flaticons  -->
+	<link href="<?php bloginfo('template_directory'); ?>/css/style.css"
+rel="stylesheet">
+
+	<!-- Theme style  -->
+
+	<!-- Modernizr JS -->
+	<script src="<?php bloginfo('template_directory'); ?>/js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+		
+	<div class="colorlib-loader"></div>
+
+	<div id="page">
+		<nav class="colorlib-nav" role="navigation">
+			<div class="top-menu">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-xs-2">
+							<div id="colorlib-logo"><a href="index.html">Article.</a></div>
+						</div>
+						<div class="col-xs-10 text-right menu-1">
+							<ul>
+								<li class="active"><a href="index.html">Home</a></li>
+								<li class="has-dropdown">
+									<a href="blog.html">Blog</a>
+									<ul class="dropdown">
+										<li><a href="#">Commercial</a></li>
+										<li><a href="#">Apartment</a></li>
+										<li><a href="#">House</a></li>
+										<li><a href="#">Building</a></li>
+									</ul>
+								</li>
+								<li><a href="lifestyle.html">Lifestyle</a></li>
+								<li><a href="travels.html">Travels</a></li>
+								<li><a href="gallery.html">Gallery</a></li>
+								<li><a href="about.html">About Me</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
+		<aside id="colorlib-hero">
+			<div class="flexslider">
+				<ul class="slides">
+			   	<li style="background-image: url(images/img_bg_1.jpg);">
+			   		<div class="overlay"></div>
+			   		<div class="container-fluid">
+			   			<div class="row">
+				   			<div class="col-md-6 col-sm-12 col-md-offset-3 slider-text">
+				   				<div class="slider-text-inner">
+				   					<div class="desc">
+					   					<h1><span class="text-center"><small>1</small></span> Top 5 Places To Start Your Vacation This Summer</h1>
+					   					<h2><span>January 20, 2017</span></h2>
+				   					</div>
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>
+
+			   	<li style="<?php bloginfo('template_directory'); ?>/background-image: url(images/img_bg_2.jpg);">
+			   		<div class="overlay"></div>
+			   		<div class="container-fluid">
+			   			<div class="row">
+				   			<div class="col-md-6 col-sm-12 col-md-offset-3 slider-text">
+				   				<div class="slider-text-inner">
+				   					<div class="desc">
+					   					<h1><span class="text-center"><small>2</small></span>  6 Facts - The Health Benefits of Swimming &amp; Surfing</h1>
+					   					<h2><span>January 20, 2017</span></h2>
+				   					</div>
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>
+			   	<li style="background-image: url(images/img_bg_3.jpg);">
+			   		<div class="overlay"></div>
+			   		<div class="container-fluid">
+			   			<div class="row">
+				   			<div class="col-md-6 col-sm-12 col-md-offset-3 slider-text">
+				   				<div class="slider-text-inner">
+				   					<div class="desc">
+					   					<h1><span class="text-center"><small>2</small></span>  Popular Lifestyle Tips</h1>
+					   					<h2><span>January 20, 2017</span></h2>
+				   					</div>
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>
+			   	<?php bloginfo('template_directory'); ?>background-image: url(images/img_bg_4.jpg);
+
+			   	<li style="<?php bloginfo('template_directory'); ?>background-image: url(images/img_bg_4.jpg);">
+			   		<div class="overlay"></div>
+			   		<div class="container-fluid">
+			   			<div class="row">
+				   			<div class="col-md-6 col-sm-12 col-md-offset-3 slider-text">
+				   				<div class="slider-text-inner">
+				   					<div class="desc">
+					   					<h1><span class="text-center"><small>2</small></span>  7 Tricks of Skateboarding For A Beginner</h1>
+					   					<h2><span>January 20, 2017</span></h2>
+				   					</div>
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>
+			  	</ul>
+		  	</div>
+		</aside>
