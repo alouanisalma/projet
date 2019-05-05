75c8d841473c9d7d7a072a3ccc4a69869cc87da0
sondes <sondesboussif1997@gmail.com>
2019-05-05 18:16:15 +0100

    affectations module aux enseigants

--- content.php
+++ content.php
@@ -4,5 +4,119 @@ the_title(); ?></a></h2>
  <p class="blog-post-meta"><?php the_date(); ?> par <a
 href="#"><?php the_author(); ?></a></p>
  <?php the_excerpt(); ?>
+ 
+
 </div><!-- /.blog-post -->
 <a href="#" class="instagram text-center" style="<?php bloginfo('template_directory'); ?>background-image: url(images/p.jpg);">
+<html>
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<title>Enseignant</title>
+</head>
+
+<body>
+
+<table border="1" width="94%" height="398">
+<caption><b><font size="6">Affectations des modules aux Enseignants</font></b></caption>
+	<tr>
+		<td width="233"><font color="#ff0000"><b>Enseignant</b></font></td>
+		<td width="155"><b><font color="#FF0000">Matière</font></b></td>
+		<td width="154"><font color="#ff0000"><b>Niveau</b></font></td>
+		<td width="173"><font color="#FF0000"><b>jour 1</b></font></td>
+		<td width="172"><font color="#ff0000"><b>Charge horaire</b></font></td>
+		<td width="166"><font color="#FF0000"><b>jour 2</b></font></td>
+		<td><font color="#ff0000"><b>Charge horaire</b></font></td>
+	</tr>
+	<tr>
+		<td width="233"><font color="#000099">Mme Manel Aloumi </font> </td>
+		<td width="155">
+		<p align="center">Pooa</td>
+		<td width="154">
+		<p align="center">dsi21</td>
+		<td width="173">
+		<p align="center">vendredi </td>
+		<td width="172">
+		<p align="center">3h30min </td>
+		<td width="166">
+		<p align="center">samedi </td>
+		<td align="center">1h30min</td>
+	</tr>
+	<tr>
+		<td width="233"><font color="#0033CC">Mrs Ramzi </font> </td>
+		<td width="155" align="center">web</td>
+		<td width="154" align="center">ti13</td>
+		<td width="173" align="center">lundi </td>
+		<td width="172" align="center">1h30min</td>
+		<td width="166" align="center">mardi </td>
+		<td align="center">3h30min</td>
+	</tr>
+	<tr>
+		<td width="233"><font color="#0033CC">Mme Hejer Bacouch </font> </td>
+		<td width="155" align="center">génie logiciel </td>
+		<td width="154" align="center">dsi21</td>
+		<td width="173" align="center">mardi</td>
+		<td width="172" align="center">1h30min</td>
+		<td width="166" align="center">vendredi </td>
+		<td align="center">1h30min</td>
+	</tr>
+	<tr>
+		<td width="233"><font color="#0033CC">Mrs Amer Zaydoun </font> </td>
+		<td width="155" align="center">réseau</td>
+		<td width="154" align="center">rsi21</td>
+		<td width="173" align="center">jeudi</td>
+		<td width="172" align="center">1h30min</td>
+		<td width="166" align="center">samedi</td>
+		<td align="center">3h30min</td>
+	</tr>
+	<tr>
+		<td width="233"><font color="#0033CC">Mme sana zakraoui </font> </td>
+		<td width="155" align="center">réseau</td>
+		<td width="154" align="center">ti13</td>
+		<td width="173" align="center">mercredi</td>
+		<td width="172" align="center">3h30min</td>
+		<td width="166" align="center">jeudi</td>
+		<td align="center">1h30min </td>
+	</tr>
+	<tr>
+		<td width="233" height="26"><font color="#0033CC">Mme Ikram chebi</font></td>
+		<td width="155" height="26" align="center">SGBD</td>
+		<td width="154" height="26" align="center">dsi21</td>
+		<td width="173" height="26" align="center">lundi</td>
+		<td width="172" height="26" align="center">3h30min</td>
+		<td width="166" height="26" align="center">lundi</td>
+		<td height="26" align="center">1h30min</td>
+	</tr>
+	<tr>
+		<td width="233" height="48"><font color="#0033CC">Mrs Mohamed Saleh Medeb</font></td>
+		<td width="155" height="48" align="center">CSNA</td>
+		<td width="154" height="48" align="center">RS31</td>
+		<td width="173" height="48" align="center">Vendredi</td>
+		<td width="172" height="48" align="center">3h30min</td>
+		<td width="166" height="48" align="center">mercredi</td>
+		<td height="48" align="center">1h30min </td>
+	</tr>
+	<tr>
+		<td width="233"><font color="#0033CC">Mohamed Taher ben Bahri</font></td>
+		<td width="155" align="center">administration système</td>
+		<td width="154" align="center">RSI21</td>
+		<td width="173" align="center">vendredi</td>
+		<td width="172" align="center">3h30min</td>
+		<td width="166" align="center">samedi</td>
+		<td align="center">1h30min</td>
+	</tr>
+	<tr>
+		<td width="233"><font color="#0033CC">Mme fatma</font></td>
+		<td width="155" align="center">Algorithme</td>
+		<td width="154" align="center">ti12</td>
+		<td width="173" align="center">vendredi</td>
+		<td width="172" align="center">1h30min</td>
+		<td width="166" align="center">jeudi</td>
+		<td align="center">3h30min</td>
+	</tr>
+
+</table>
+
+</body>
+
+</html>
