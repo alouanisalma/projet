b3b3caf0d45c54417c6057e662f2db5d1c0e24f3
sondes <sondesboussif1997@gmail.com>
2019-05-16 11:58:06 +0100

    ajout articles

--- content.php
+++ content.php
@@ -1,12 +1,11 @@
 <div class="blog-post">
  <h2 class="blog-post-title"><a href="<?php the_permalink(); ?>"><?php
 the_title(); ?></a></h2>
- <p class="blog-post-meta"><?php the_date(); ?> par <a
-href="#"><?php the_author(); ?></a></p>
+ <p class="blog-post-meta"><?php the_date(); ?> par <ahre="#"><?php the_author(); ?></a></p>
  <?php the_excerpt(); ?>
 </div><!-- /.blog-post -->
 
-<a href="#" class="instagram text-center" style="<?php bloginfo('template_directory'); ?>background-image: url(images/p.jpg);">
+<a href="#" class="instagram text-center" style="<?php bloginfo('template_directory'); ?>background-image: url(images/java.jpg);">
 <html>
 
 <head>
